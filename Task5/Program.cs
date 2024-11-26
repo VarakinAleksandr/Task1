381ece4c828c4eda44cbeeeb0f275c1215f63538
VarakinAleksandr <sanek-varakin@mail.ru>
2024-11-26 22:17:00 +0400

    add lesson1: Task5_1

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,14 +1,16 @@
 ﻿int n = 10;
 //int i =1;
-for (int i=1; i<=n;i++)
+for (int i = 1; i <= n; i++)
 {
     Console.Write(i);
+    Console.Write(' ');
 }
 
 int n1 = 10;
-int i1 =1;
-while (i1 < n1)
+int i1 = 1;
+while (i1 <= n1)
 {
     Console.Write(i1);
+    Console.Write(' ');
     i1++;
-} 
+}
