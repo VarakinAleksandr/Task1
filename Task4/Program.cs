cceb0f533733558e1dcd799511cf3f31e342b4ab
VarakinAleksandr <sanek-varakin@mail.ru>
2024-11-26 20:46:00 +0400

    add lesson1: Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 4;
+int b = 28;
+int c = 8;
+int d = 16;
+int e = 45;
+int max = a;
+
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+if (d>max)
+{
+    max = d;
+}
+if (e>max)
+{
+    max = e;
+}
+Console.WriteLine(max);
